f48d04b295b646d09946e75da0fee15335e8de99
Hallysson Ribeiro <hallyssonribeiro2001@gmail.com>
2021-03-18 18:04:33 -0300

    Created new page/Links/Aboutme

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
-  };
-  xhttp.open("GET", "a.html", true);
-  xhttp.send();
-}
