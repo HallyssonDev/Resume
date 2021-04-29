85017ad1a8f6e66f1f6a3a581830bc1adf0cbb57
Hallysson <hallyssonrdev@gmail.com>
2021-04-29 11:40:09 -0300

    Cards added in Projects

--- View/pages/projects/projects.php
+++ View/pages/projects/projects.php
@@ -7,7 +7,53 @@
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/card.min.css">
 </head>
 <body>
-	
+	<div class="hls-cards">
+		<!-- First Column -->
+		<div class="card">
+			<a href="">
+				<img class="hls-img" src=""> <!-- pages/projects/imgs/gamingo.jpeg -->
+			</a>
+			<div class="card-description">
+				<h4>Gamin' Go</h4>
+				<p>Este foi um projeto colaborativo que fiz com um amigo</p>
+			</div>
+		</div>
+
+		<!-- Second Column -->
+
+		<div class="card">
+			<a href="">
+				<img src="">
+			</a>
+			<div class="card-description">
+				<h4>CrudPHP</h4>
+				<p>Este foi um projeto colaborativo que fiz com um amigo</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="hls-cards">
+		<!-- Second Row/First Column -->
+		<div class="card">
+			<a href="">
+				<img src="">
+			</a>
+			<div class="card-description">
+				<h4>CrudPHP</h4>
+				<p>Este foi um projeto colaborativo que fiz com um amigo</p>
+			</div>
+		</div>
+		<!-- Second Row/Second Column -->
+		<div class="card">
+			<a href="">
+				<img src="">
+			</a>
+			<div class="card-description">
+				<h4>CrudPHP</h4>
+				<p>Este foi um projeto colaborativo que fiz com um amigo</p>
+			</div>
+		</div>
+	</div>
 	
 
 </body>
